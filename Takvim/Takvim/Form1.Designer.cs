536efb1cb576629757cc538713e32373bd6f1213
fUuunN <95917480+fUuunN@users.noreply.github.com>
2023-04-24 17:59:05 +0300

    Login Ekranı Oluşturuluyor.
    Kullanıcı adı , şifre boxları oluşturuldu.

--- Takvim/Takvim/Form1.Designer.cs
+++ Takvim/Takvim/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(115, 144);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(152, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Enter += textBox1_Enter;
+            textBox1.Leave += textBox1_Leave;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(115, 197);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(152, 23);
+            textBox2.TabIndex = 1;
+            textBox2.Enter += textBox2_Enter;
+            textBox2.Leave += textBox2_Leave;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(192, 293);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(115, 244);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(115, 15);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Şifremi unuttum pls!";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(369, 419);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private LinkLabel linkLabel1;
     }
 }
--- Takvim/Takvim/Form1.cs
+++ Takvim/Takvim/Form1.cs
@@ -6,5 +6,41 @@ namespace Takvim
         {
             InitializeComponent();
         }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Username")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Red;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Username";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Password")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Red;
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Password";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
     }
 }
